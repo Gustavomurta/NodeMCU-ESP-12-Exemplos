8d9959fc574b906dd9adfba332ff1dd19600eb6f
Jose Gustavo Abreu Murta <jgustavoam@gmail.com>
2018-03-07 07:14:45 -0300

    Add files via upload
    Sketch para ESP8266, para pesquisar Redes WIFI ( próximas ao NodeMCU) .

---
+++ ESP8266_ScanWIFI.ino.ino
@@ -0,0 +1,26 @@
+#include "ESP8266WiFi.h"
+
+void setup()
+{
+  Serial.begin(115200);      // configura monitor serial 115200 Bps
+  Serial.println();          // imprime uma linha 
+  WiFi.mode(WIFI_STA);       // configura rede no modo estacao 
+  WiFi.disconnect();         // desconecta rede WIFI
+  delay(100);                // atraso de 100 milisegundos
+}
+
+void prinScanResult(int networksFound)
+{
+  Serial.printf("\n");                                      // imprime uma linha 
+  Serial.printf("%d redes encontradas\n", networksFound);   // imprime numero de redes encontradas
+  for (int i = 0; i < networksFound; i++)                   // contagem das redes encontradas
+  {
+    Serial.printf("%d: %s, Ch:%d (%ddBm) %s\n", i + 1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "aberta" : "");
+  }
+}
+
+void loop()
+{
+  WiFi.scanNetworksAsync(prinScanResult);        // imprime o resultado 
+  delay(500);                                    // atraso de 0,5 segundos
+}
